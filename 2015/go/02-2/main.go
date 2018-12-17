7e8ae5cebcb8f55a8de8477038aba1b0b5125cbe
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2018-12-17 17:23:22 +0300

    2015/go: add solution for problem 02-2
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

---
+++ 2015/go/02-2/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+
+	"github.com/pborzenkov/aoc/pkg/go/input"
+)
+
+func max(val int, vals ...int) int {
+	mv := val
+	for _, v := range vals {
+		if v > mv {
+			mv = v
+		}
+	}
+	return mv
+}
+
+func main() {
+	r := input.NewFileOrStdin()
+	defer r.Close()
+
+	s := bufio.NewScanner(r)
+
+	var total uint64
+	for s.Scan() {
+		var w, h, l int
+
+		_, err := fmt.Sscanf(s.Text(), "%dx%dx%d", &w, &h, &l)
+		if err != nil {
+			log.Fatalf("Could not fmt.Sscanf line %q: %v", s.Text(), err)
+		}
+
+		total += uint64((w+h+l-max(w, h, l))*2 + w*h*l)
+	}
+	if s.Err() != nil {
+		log.Fatalf("Scanning completed with error (%v), result might be unreliable", s.Err())
+	}
+	log.Printf("Total feet of ribbon required: %v", total)
+}
