1725fdc4f9b1d4747a65484e8dbe07136ec4809b
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2018-12-18 15:09:09 +0300

    2015/go: add solution for problem 03-2
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

---
+++ 2015/go/03-2/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"log"
+
+	"github.com/pborzenkov/aoc/pkg/go/input"
+)
+
+func main() {
+	r := input.NewFileOrStdin()
+
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanRunes)
+
+	type point struct {
+		x, y int
+	}
+
+	var curPos [2]point
+	path := make(map[point]struct{})
+	path[point{}] = struct{}{}
+
+	token := 0
+	for s.Scan() {
+		p := &curPos[token%2]
+
+		switch s.Text() {
+		case ">":
+			p.x += 1
+		case "<":
+			p.x -= 1
+		case "^":
+			p.y += 1
+		case "v":
+			p.y -= 1
+		default:
+			log.Fatalf("Unsupported input token %q", s.Text())
+		}
+		path[*p] = struct{}{}
+		token += 1
+	}
+	if s.Err() != nil {
+		log.Fatalf("Scanning completed with error (%v), result might be unreliable", s.Err())
+	}
+	log.Printf("%d houses will receive at least one present", len(path))
+}
