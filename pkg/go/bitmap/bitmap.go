3597be2b89b3c69726e772f29c4d78243f89e35e
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2019-02-10 22:33:49 +0300

    pkg/go: add simple bitmap package
    For now, implement only methods needed to solve 2015/6-1
    
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

---
+++ pkg/go/bitmap/bitmap.go
@@ -0,0 +1,166 @@
+package bitmap
+
+import (
+	"errors"
+	"fmt"
+	"math/bits"
+	"strconv"
+	"strings"
+)
+
+var (
+	// ErrOutOfBounds is returned when a bitmap operation results in out-of-bound access.
+	ErrOutOfBounds = errors.New("out of bounds")
+	// ErrMalformedInput is returned when a string to bitmap conversion fails
+	ErrMalformedInput = errors.New("malformed input")
+)
+
+const wordSize = bits.UintSize
+
+// Bitmap represents a single bitmap.
+type Bitmap struct {
+	size uint
+	data []uint
+}
+
+// New returns a new instance of a bitmap capable of holding up to size bits.
+// The bitmap is intialized with zeros.
+func New(size uint) *Bitmap {
+	words := (size + wordSize - 1) / wordSize
+
+	return &Bitmap{
+		size: size,
+		data: make([]uint, words),
+	}
+}
+
+// FromString returns a bitmap from its string representation
+func FromString(s string) (*Bitmap, error) {
+	b := New(uint(len(s)))
+	r := strings.NewReader(s)
+	bitsToScan := b.size % wordSize
+	if bitsToScan == 0 {
+		bitsToScan = wordSize
+	}
+
+	for i := len(b.data) - 1; i >= 0; i-- {
+		buf := make([]byte, bitsToScan)
+		if n, err := r.Read(buf); n != len(buf) || err != nil {
+			return nil, ErrMalformedInput
+		}
+		n, err := strconv.ParseUint(string(buf), 2, 64)
+		if err != nil {
+			return nil, ErrMalformedInput
+		}
+		b.data[i] = uint(n)
+		bitsToScan = wordSize
+	}
+
+	return b, nil
+}
+
+// String returns a string representation of the bitmap (least significant bit
+// is on the right).
+func (b *Bitmap) String() string {
+	w := new(strings.Builder)
+	bitsToPrint := b.size % wordSize
+	if bitsToPrint == 0 {
+		bitsToPrint = wordSize
+	}
+
+	for i := len(b.data) - 1; i >= 0; i-- {
+		fmt.Fprintf(w, "%0[2]*[1]b", b.data[i], bitsToPrint)
+		bitsToPrint = wordSize
+	}
+
+	return w.String()
+}
+
+// Set sets nbits starting from bit start.
+func (b *Bitmap) Set(start, nbits uint) error {
+	if start >= b.size || (start+nbits) > b.size || (start+nbits < start) {
+		return ErrOutOfBounds
+	}
+
+	i := start / wordSize
+	bitsToSet := int(wordSize - start%wordSize)
+	mask := ^uint(0) << (start & (wordSize - 1))
+
+	l := int(nbits)
+	for l-bitsToSet >= 0 {
+		b.data[i] |= mask
+		l -= bitsToSet
+		bitsToSet = wordSize
+		mask = ^uint(0)
+		i++
+	}
+	if l > 0 {
+		mask &= ^uint(0) >> (-(start + nbits) & (wordSize - 1))
+		b.data[i] |= mask
+	}
+
+	return nil
+}
+
+// Clear clears nbits starting from bit start.
+func (b *Bitmap) Clear(start, nbits uint) error {
+	if start >= b.size || (start+nbits) > b.size || (start+nbits < start) {
+		return ErrOutOfBounds
+	}
+
+	i := start / wordSize
+	bitsToClear := int(wordSize - start%wordSize)
+	mask := ^uint(0) << (start & (wordSize - 1))
+
+	l := int(nbits)
+	for l-bitsToClear >= 0 {
+		b.data[i] &^= mask
+		l -= bitsToClear
+		bitsToClear = wordSize
+		mask = ^uint(0)
+		i++
+	}
+	if l > 0 {
+		mask &= ^uint(0) >> (-(start + nbits) & (wordSize - 1))
+		b.data[i] &^= mask
+	}
+
+	return nil
+}
+
+// Not applies bitwise NOT operation to nbits starting from start.
+func (b *Bitmap) Not(start, nbits uint) error {
+	if start >= b.size || (start+nbits) > b.size || (start+nbits < start) {
+		return ErrOutOfBounds
+	}
+
+	i := start / wordSize
+	bitsToNot := int(wordSize - start%wordSize)
+	mask := ^uint(0) << (start & (wordSize - 1))
+
+	l := int(nbits)
+	for l-bitsToNot >= 0 {
+		b.data[i] ^= mask
+		l -= bitsToNot
+		bitsToNot = wordSize
+		mask = ^uint(0)
+		i++
+	}
+	if l > 0 {
+		mask &= ^uint(0) >> (-(start + nbits) & (wordSize - 1))
+		b.data[i] ^= mask
+	}
+
+	return nil
+}
+
+// Weight returns a Hamming weight of the bitmap (number of set bits).
+func (b *Bitmap) Weight() int {
+	var weight int
+
+	for _, d := range b.data {
+		weight += bits.OnesCount(d)
+	}
+
+	return weight
+}
---
+++ pkg/go/bitmap/bitmap_test.go
@@ -0,0 +1,191 @@
+package bitmap_test
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/pborzenkov/aoc/pkg/go/bitmap"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFromString(t *testing.T) {
+	var tests = []struct {
+		input string
+		err   error
+	}{
+		{"001", nil},
+		{"00000010000000000000000000010000", nil},
+		{"000000000000001000000000000000100", nil},
+		{"0010000011010000111000000000000000000000000000000000000000000000", nil},
+		{"00000111000000000000000000000000000000000000000000000000000001111", nil},
+		{"", nil},
+		{"00010110110101a0011011", bitmap.ErrMalformedInput},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test_%d", i+1), func(t *testing.T) {
+			bmap, err := bitmap.FromString(test.input)
+			if test.err != nil {
+				assert.Equal(t, test.err, err)
+				return
+			}
+			require.NoError(t, err)
+			assert.Equal(t, test.input, bmap.String())
+		})
+	}
+}
+
+func TestString(t *testing.T) {
+	var tests = []struct {
+		get func() *bitmap.Bitmap
+		str string
+	}{
+		{func() *bitmap.Bitmap { return bitmap.New(3) }, "000"},
+		{func() *bitmap.Bitmap { return bitmap.New(32) }, "00000000000000000000000000000000"},
+		{func() *bitmap.Bitmap { return bitmap.New(33) }, "000000000000000000000000000000000"},
+		{func() *bitmap.Bitmap { return bitmap.New(64) },
+			"0000000000000000000000000000000000000000000000000000000000000000"},
+		{func() *bitmap.Bitmap { return bitmap.New(65) },
+			"00000000000000000000000000000000000000000000000000000000000000000"},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test_%d", i+1), func(t *testing.T) {
+			bmap := test.get()
+			assert.Equal(t, test.str, bmap.String())
+		})
+	}
+}
+
+func TestSet(t *testing.T) {
+	var size uint = 65
+
+	var tests = []struct {
+		start uint
+		size  uint
+		err   error
+		str   string
+	}{
+		{0, 65, nil, "11111111111111111111111111111111111111111111111111111111111111111"},
+		{0, 16, nil, "00000000000000000000000000000000000000000000000001111111111111111"},
+		{60, 5, nil, "11111000000000000000000000000000000000000000000000000000000000000"},
+		{64, 1, nil, "10000000000000000000000000000000000000000000000000000000000000000"},
+		{65, 1, bitmap.ErrOutOfBounds, ""},
+		{65, 0, bitmap.ErrOutOfBounds, ""},
+		{30, 0, nil, "00000000000000000000000000000000000000000000000000000000000000000"},
+		{62, 3, nil, "11100000000000000000000000000000000000000000000000000000000000000"},
+		{15, math.MaxUint64, bitmap.ErrOutOfBounds, ""},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test_%d", i+1), func(t *testing.T) {
+			bmap := bitmap.New(size)
+			err := bmap.Set(test.start, test.size)
+			if test.err != nil {
+				assert.Equal(t, test.err, err)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, test.str, bmap.String())
+		})
+	}
+}
+
+func TestClear(t *testing.T) {
+	var size uint = 65
+
+	var tests = []struct {
+		start uint
+		size  uint
+		err   error
+		str   string
+	}{
+		{0, 65, nil, "00000000000000000000000000000000000000000000000000000000000000000"},
+		{0, 16, nil, "11111111111111111111111111111111111111111111111110000000000000000"},
+		{60, 5, nil, "00000111111111111111111111111111111111111111111111111111111111111"},
+		{64, 1, nil, "01111111111111111111111111111111111111111111111111111111111111111"},
+		{65, 1, bitmap.ErrOutOfBounds, ""},
+		{65, 0, bitmap.ErrOutOfBounds, ""},
+		{30, 0, nil, "11111111111111111111111111111111111111111111111111111111111111111"},
+		{62, 3, nil, "00011111111111111111111111111111111111111111111111111111111111111"},
+		{15, math.MaxUint64, bitmap.ErrOutOfBounds, ""},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test_%d", i+1), func(t *testing.T) {
+			bmap := bitmap.New(size)
+			require.NoError(t, bmap.Set(0, size))
+			err := bmap.Clear(test.start, test.size)
+			if test.err != nil {
+				assert.Equal(t, test.err, err)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, test.str, bmap.String())
+		})
+	}
+}
+
+func TestNot(t *testing.T) {
+	var tests = []struct {
+		input string
+		start uint
+		size  uint
+		err   error
+		str   string
+	}{
+		{
+			"10101010101010101010101010101010101010101010101010101010101010101",
+			0, 65, nil,
+			"01010101010101010101010101010101010101010101010101010101010101010",
+		},
+		{
+			"10101010101010101010101010101010101010101010101010101010101010101",
+			65, 1, bitmap.ErrOutOfBounds, "",
+		},
+		{
+			"10101010101010101010101010101010101010101010101010101010101010101",
+			65, 0, bitmap.ErrOutOfBounds, "",
+		},
+		{
+			"10101010101010101010101010101010101010101010101010101010101010101",
+			15, math.MaxUint64, bitmap.ErrOutOfBounds, "",
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test_%d", i+1), func(t *testing.T) {
+			bmap, err := bitmap.FromString(test.input)
+			require.NoError(t, err)
+			err = bmap.Not(test.start, test.size)
+			if test.err != nil {
+				assert.Equal(t, test.err, err)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, test.str, bmap.String())
+		})
+	}
+}
+
+func TestWeight(t *testing.T) {
+	var tests = []struct {
+		input  string
+		weight int
+	}{
+		{"10101010101010101010101010101010101010101010101010101010101010101", 33},
+		{"00000000000000000000000000000000000000000000000000000000000000000", 0},
+		{"11111111111111111111111111111111111111111111111111111111111111111", 65},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test_%d", i+1), func(t *testing.T) {
+			bmap, err := bitmap.FromString(test.input)
+			require.NoError(t, err)
+			weight := bmap.Weight()
+			assert.Equal(t, test.weight, weight)
+		})
+	}
+}
