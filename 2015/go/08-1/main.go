7677828bfa7e4b36afb4e8ff964ff0ac0a14b331
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2019-04-12 18:34:26 +0300

    2015/go: add solution for problem 08-1
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

---
+++ 2015/go/08-1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"strconv"
+
+	"github.com/pborzenkov/aoc/pkg/go/input"
+)
+
+func main() {
+	r := input.NewFileOrStdin()
+	s := bufio.NewScanner(r)
+
+	var codeLen, memLen int
+	for s.Scan() {
+		str := s.Text()
+
+		codeLen += len(str)
+		unquoted, err := strconv.Unquote(str)
+		if err != nil {
+			log.Fatalf("Failed to unquote %q: %v", str, err)
+		}
+		memLen += len(unquoted)
+	}
+	if s.Err() != nil {
+		log.Fatalf("Scanning completed with error (%v), result might be unreliable", s.Err())
+	}
+
+	log.Printf("Difference: %d", codeLen-memLen)
+}
