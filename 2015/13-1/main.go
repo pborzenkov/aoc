4cca71ec40150170a3da5f178ec89ccb4ab206ca
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2020-03-19 11:30:39 +0300

    2015: add solutions for 13-1 and 13-2
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

---
+++ 2015/13-1/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"regexp"
+	"strconv"
+
+	"github.com/pborzenkov/aoc/pkg/input"
+	"github.com/pborzenkov/aoc/pkg/iterutils"
+)
+
+var (
+	reSitting = regexp.MustCompile(`(\w+) would (\w+) (\d+) happiness units by sitting next to (\w+)\.`)
+
+	happiness = make(map[string]map[string]int)
+)
+
+func setHappiness(who string, nextTo string, d int) {
+	if _, ok := happiness[who]; !ok {
+		happiness[who] = make(map[string]int)
+	}
+	happiness[who][nextTo] = d
+}
+
+func main() {
+	r := input.NewFileOrStdin()
+	s := bufio.NewScanner(r)
+
+	for s.Scan() {
+		groups := reSitting.FindStringSubmatch(s.Text())
+		if groups == nil {
+			log.Fatalf("failed to parse sitting spec %q", s.Text())
+		}
+
+		h, err := strconv.Atoi(groups[3])
+		if err != nil {
+			log.Fatalf("failed to parse distance %q: %v", groups[3], err)
+		}
+		if groups[2] == "lose" {
+			h = -h
+		}
+
+		setHappiness(groups[1], groups[4], h)
+	}
+
+	persons := make([]string, 0, len(happiness))
+	for i := range happiness {
+		persons = append(persons, i)
+	}
+
+	left := func(i int) int {
+		if i == 0 {
+			return len(persons) - 1
+		}
+		return i - 1
+	}
+	right := func(i int) int {
+		if i == len(persons)-1 {
+			return 0
+		}
+		return i + 1
+	}
+
+	max := 0
+	iterutils.ForAllPerms(persons, func(persons []string) {
+		var d int
+
+		for i := 0; i < len(persons); i++ {
+			d += happiness[persons[i]][persons[left(i)]]
+			d += happiness[persons[i]][persons[right(i)]]
+		}
+
+		if d > max {
+			max = d
+		}
+	})
+	log.Printf("Max happiness is %d", max)
+}
